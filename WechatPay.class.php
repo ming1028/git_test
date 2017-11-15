75ee055a84ed924922ab83572955eabdc00c28c2
ming1028 <13030365596@163.com>
2017-11-15 18:21:59 +0800

    wechatPay
    微信支付

---
+++ WechatPay.class.php
@@ -0,0 +1,420 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: echo
+ * Date: 2017/11/15
+ * Time: 10:22
+ */
+namespace Think;
+class WechatPay
+{
+    public $values = array();
+    //调用方法
+    public function getParam()
+    {
+
+    }
+
+    /**
+     * 设置商品或支付单简要描述
+     * @param string $value
+     **/
+    public function SetBody($value)
+    {
+        $this->values['body'] = $value;
+    }
+
+    /**
+     * 设置附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
+     * @param string $value
+     **/
+    public function SetAttach($value)
+    {
+        $this->values['attach'] = $value;
+    }
+
+    /**
+     * 设置商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
+     * @param string $value
+     **/
+    public function SetOut_trade_no($value)
+    {
+        $this->values['out_trade_no'] = $value;
+    }
+
+    /**
+     * 设置订单总金额，只能为整数，详见支付金额
+     * @param string $value
+     **/
+    public function SetTotal_fee($value)
+    {
+        $this->values['total_fee'] = $value;
+    }
+
+    /**
+     * 设置订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010。其他详见时间规则
+     * @param string $value
+     **/
+    public function SetTime_start($value)
+    {
+        $this->values['time_start'] = $value;
+    }
+
+    /**
+     * 设置订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010。其他详见时间规则
+     * @param string $value
+     **/
+    public function SetTime_expire($value)
+    {
+        $this->values['time_expire'] = $value;
+    }
+
+    /**
+     * 设置商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
+     * @param string $value
+     **/
+    public function SetGoods_tag($value)
+    {
+        $this->values['goods_tag'] = $value;
+    }
+
+    /**
+     * 设置接收微信支付异步通知回调地址
+     * @param string $value
+     **/
+    public function SetNotify_url($value)
+    {
+        $this->values['notify_url'] = $value;
+    }
+
+    /**
+     * 设置取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
+     * @param string $value
+     **/
+    public function SetTrade_type($value)
+    {
+        $this->values['trade_type'] = $value;
+    }
+
+    /**
+     * 设置trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识。下单前需要调用【网页授权获取用户信息】接口获取到用户的Openid。
+     * @param string $value
+     **/
+    public function SetOpenid($value)
+    {
+        $this->values['openid'] = $value;
+    }
+
+    /**
+     * 设置微信分配的公众账号ID
+     * @param string $value
+     **/
+    public function SetAppid($value)
+    {
+        $this->values['appid'] = $value;
+    }
+
+    /**
+     * 设置微信支付分配的商户号
+     * @param string $value
+     **/
+    public function SetMch_id($value)
+    {
+        $this->values['mch_id'] = $value;
+    }
+
+    /**
+     * 设置APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
+     * @param string $value
+     **/
+    public function SetSpbill_create_ip($value)
+    {
+        $this->values['spbill_create_ip'] = $value;
+    }
+
+    /**
+     * 设置随机字符串，不长于32位。推荐随机数生成算法
+     * @param string $value
+     **/
+    public function SetNonce_str($value)
+    {
+        $this->values['nonce_str'] = $value;
+    }
+
+
+    /**
+     *
+     * 产生随机字符串，不长于32位
+     * @param int $length
+     * @return 产生的随机字符串
+     */
+    public function getNonceStr($length = 32)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str ="";
+        for ( $i = 0; $i < $length; $i++ )  {
+            $str .= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+        }
+        return $str;
+    }
+
+    /**
+     * 设置签名，详见签名生成算法
+     * @param string $value
+     **/
+    public function SetSign($key)
+    {
+        $sign = $this->MakeSign($key);
+        $this->values['sign'] = $sign;
+        return $sign;
+    }
+
+    /**
+     * 生成签名
+     * @return 签名，本函数不覆盖sign成员变量，如要设置签名需要调用SetSign方法赋值
+     */
+    public function MakeSign($key)
+    {
+        //签名步骤一：按字典序排序参数
+        ksort($this->values);
+        $string = $this->ToUrlParams();
+        //签名步骤二：在string后加入KEY
+        $string = $string . "&key=".$key;
+        //签名步骤三：MD5加密
+        $string = md5($string);
+        //签名步骤四：所有字符转为大写
+        $result = strtoupper($string);
+        return $result;
+    }
+
+    /**
+     * 格式化参数格式化成url参数
+     */
+    public function ToUrlParams()
+    {
+        $buff = "";
+        foreach ($this->values as $k => $v)
+        {
+            if($k != "sign" && $v != "" && !is_array($v)){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    /**
+     * 输出xml字符
+     * @throws WxPayException
+     **/
+    public function ToXml()
+    {
+        if(!is_array($this->values)
+            || count($this->values) <= 0)
+        {
+            throw new Exception("数组数据异常！");
+        }
+
+        $xml = "<xml>";
+        foreach ($this->values as $key=>$val)
+        {
+            if (is_numeric($val)){
+                $xml.="<".$key.">".$val."</".$key.">";
+            }else{
+                $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
+            }
+        }
+        $xml.="</xml>";
+        return $xml;
+    }
+
+    /**
+     * 以post方式提交xml到对应的接口url
+     *
+     * @param string $xml  需要post的xml数据
+     * @param string $url  url
+     * @param bool $useCert 是否需要证书，默认不需要
+     * @param int $second   url执行超时时间，默认30s
+     * @throws WxPayException
+     */
+    public static function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        // echo '<pre>';
+        // var_dump($data);exit();
+        // header('content-type:text/xml;');
+        // var_dump($data);
+        curl_close($ch);
+        return $data;
+        //返回结果
+        // if($data){
+        // curl_close($ch);
+        // return $data;
+        // } else {
+        // $error = curl_error($ch);
+        // curl_close($ch);
+        // throw new Exception("curl出错，错误码:$error");
+        // }
+    }
+
+    /**
+     * 设置签名方式
+     * @param string $value
+     **/
+    public function SetPaySign($value)
+    {
+        $this->values['paySign'] = $value;
+    }
+
+    /**
+     * 设置签名方式
+     * @param string $value
+     **/
+    public function SetSignType($value)
+    {
+        $this->values['signType'] = $value;
+    }
+
+    /**
+     * 设置订单详情扩展字符串
+     * @param string $value
+     **/
+    public function SetPackage($value)
+    {
+        $this->values['package'] = $value;
+    }
+
+    /**
+     * 随机字符串
+     * @param string $value
+     **/
+    public function SetNonceStr($value)
+    {
+        $this->values['nonceStr'] = $value;
+    }
+
+    /**
+     * 设置支付时间戳
+     * @param string $value
+     **/
+    public function SetTimeStamp($value)
+    {
+        $this->values['timeStamp'] = $value;
+    }
+
+    /**
+     * 获取毫秒级别的时间戳
+     */
+    public static function getMillisecond()
+    {
+        //获取毫秒的时间戳
+        $time = explode ( " ", microtime () );
+        $time = $time[1] . ($time[0] * 1000);
+        $time2 = explode( ".", $time );
+        $time = $time2[0];
+        return $time;
+    }
+
+    /**
+     * 将xml转为array
+     * @param string $xml
+     * @throws WxPayException
+     */
+    public static function Init($xml,$key)
+    {
+        $obj = new self();
+        $obj->FromXml($xml);
+        //fix bug 2015-06-29
+        if($obj->values['return_code'] != 'SUCCESS'){
+            return $obj->GetValues();
+        }
+        $obj->CheckSign($key);
+        return $obj->GetValues();
+    }
+
+    /**
+     * 获取设置的值
+     */
+    public function GetValues()
+    {
+        return $this->values;
+    }
+
+    /**
+     * 将xml转为array
+     * @param string $xml
+     * @throws WxPayException
+     */
+    public function FromXml($xml)
+    {
+        if(!$xml){
+            throw new Exception("xml数据异常！");
+        }
+        //将XML转为array
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+        $this->values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        // echo '<pre>';
+        // var_dump($this->values);exit();
+        return $this->values;
+    }
+
+    /**
+     *
+     * 检测签名
+     */
+    public function CheckSign($key)
+    {
+        //fix异常
+        if(!$this->IsSignSet()){
+            throw new Exception("签名错误！");
+        }
+
+        $sign = $this->MakeSign($key);
+        // echo '<pre>';
+        // var_dump($sign);
+        // var_dump($this->GetSign());
+        // exit();
+        if($this->GetSign() == $sign){
+            return true;
+        }
+        throw new Exception("签名错误！");
+    }
+
+    /**
+     * 判断签名，详见签名生成算法是否存在
+     * @return true 或 false
+     **/
+    public function IsSignSet()
+    {
+        return array_key_exists('sign', $this->values);
+    }
+
+    /**
+     * 获取签名，详见签名生成算法的值
+     * @return 值
+     **/
+    public function GetSign()
+    {
+        return $this->values['sign'];
+    }
+
+}
